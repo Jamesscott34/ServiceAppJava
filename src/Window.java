028b5c75b862d4a5a19b8abc493496cf67883b42
Jamesscott34 <jamesdeanscott19@gmail.com>
2023-12-02 10:16:16 +0000

    Update Window.java

--- src/Window.java
+++ src/Window.java
@@ -38,6 +38,7 @@ public class Window {
         reportButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
+                Report.showReportForm();
             }
         });
 
